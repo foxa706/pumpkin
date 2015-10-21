2ef622ca8f987e62dd37c4a6a3ea1475bb9fce5e
foxa706 <foxa706@newschool.edu>
2015-10-21 15:06:15 -0400

    second commit

--- pumpkin.js
+++ pumpkin.js
@@ -15,9 +15,6 @@ var port = new SerialPort("/dev/ttyAMA0", {
   parser: serialport.parsers.readline("\n")
 }, false); // this is the openImmediately flag [default is true]
 
-led = new GPIO(17, 'out');
-
-
 
 app.use(express.static(__dirname + '/'));
 console.log('Simple static server listening at '+url+':'+port);
@@ -29,39 +26,11 @@ port.open(function(error) {
     console.log('failed to open: ' + error);
   } else {
     console.log('Serial open');
-    port.on('data', function(data) {
-    //console.log('data length: ' + data.length);
-    console.log((data));
-    light(data[5]);
-    });
-}  
-});
-
-function light(pum) {
-
-  var pum = parseInt(pum);
-  console.log(pum);
-
-    if(pum < 4) {
-      console.log("Jack O Lantern is lit!");
-      led.writeSync(1);
-      //$("body").css("background-color","black");
-
-
-    } else {
-      console.log("Jack O Lantern is off.");
-      led.writeSync(0);
-      //$("body").css("background-color","#7ec0ee");
-
     }
-  }
-
-function run(){
-  light();
-}
+});
 
 });
-  
+
 
 
 
